1df6704e3159489e44584f5757f2ad02c6f36ca2
DavisAndn <davis.andherson@gmail.com>
2023-09-09 11:21:23 +0530

    feat(ui): Added basic UI

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [meters, setMeters] = useState([{lastReading: 0, currentReading: 0, lastReadingDate: ''}]);
+
+  const handleInputChange = (text, index, field) => {
+    const tempMeters = [...meters];
+    tempMeters[index][field] = text;
+    setMeters(tempMeters);
+  };
+
+  const calculateDifference = (lastReading, currentReading) => {
+    return currentReading - lastReading;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your appppp!</Text>
-      <StatusBar style="auto" />
+
+      {meters.map((meter, index) => (
+        <View key={index} style={styles.meterContainer}>
+          <Text style={styles.meterText}>Meter {index + 1}</Text>
+          <TextInput style={styles.input} placeholder="Last Reading"
+            onChangeText={text => handleInputChange(text, index, 'lastReading')}
+            value={meter.lastReading}
+          />
+          <TextInput style={styles.input} placeholder="Current Reading"
+            onChangeText={text => handleInputChange(text, index, 'currentReading')}
+            value={meter.currentReading}
+          />
+          <TextInput style={styles.input} placeholder="Last Reading Date"
+            onChangeText={text => handleInputChange(text, index, 'lastReadingDate')}
+            value={meter.lastReadingDate}
+          />
+          <Text style={styles.differenceText}>
+            Difference: {calculateDifference(meter.lastReading, meter.currentReading)}
+          </Text>
+        </View>
+      ))}
+
+      <Button title="Add More Meters" onPress={() => setMeters([...meters, {lastReading: 0, currentReading: 0, lastReadingDate: ''}])}/>
+
     </View>
   );
 }
@@ -13,8 +47,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: "#f8f8f8"
+  },
+  meterContainer: {
+    marginTop: 20,
+    borderColor: "#cccccc",
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+  meterText: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#333333'
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  differenceText:{
+    marginTop: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#FF4500'
   },
 });
